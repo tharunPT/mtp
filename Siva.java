3e45d9610088021de9c9f22486380d63335cd738
tharunPT <42170232+tharunPT@users.noreply.github.com>
2018-08-07 14:28:25 +0530

    Create Siva.java

---
+++ Siva.java
@@ -0,0 +1,23 @@
+import java.util .* ;
+class Siva
+ {
+    public static void main(String arg[])
+      {
+        int a;
+        Scanner b=new Scanner(System. in);
+        System.out.println("Enter the values");
+        a=b.nextInt();
+        if(a>0)
+          {
+            System.out.println("Positive");
+          }
+         elseif(a<0)
+          {
+           System.out.println("Negative");
+           }
+           else
+           {
+               System.out.println("zero");
+            }
+       }
+ }      
